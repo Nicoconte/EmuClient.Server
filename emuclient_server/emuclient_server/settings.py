40045198f8846ac1edf353be52ca2bde38969707
Nicolas Conte <nicoconte1999@gmail.com>
2021-11-14 01:27:48 -0300

    Change: Beautifulsoup and requests was added in Requirements.txr

--- emuclient_server/emuclient_server/settings.py
+++ emuclient_server/emuclient_server/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api.license_key'
+    'api.license_key',
+    'api.games'
 ]
 
 MIDDLEWARE = [
--- emuclient_server/emuclient_server/urls.py
+++ emuclient_server/emuclient_server/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/emuclient/license-key/', include('api.license_key.urls'))
+    path('api/emuclient/license-key/', include('api.license_key.urls')),
+    path('api/emuclient/games/', include('api.games.urls'))
 ]
